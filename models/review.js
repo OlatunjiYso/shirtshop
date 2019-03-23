7d4700b05c822d855408491f557e0873dc4ac7f4
Olatunji Yusuf <olatunjiyso@yahoo.com>
2019-03-23 21:27:27 +0100

    add review model

---
+++ models/review.js
@@ -0,0 +1,40 @@
+
+export default module.exports = (sequelize, DataTypes) => {
+  const Review = sequelize.define('Review', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
+    customerId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    productId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    review: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  }, {});
+  Review.associate = (models) => {
+    Review.belongsTo(models.Customer, {
+      foreignKey: 'customerId',
+      onDelete: 'CASCADE',
+      as: 'customer'
+    });
+    Review.belongsTo(models.Product, {
+      foreignKey: 'productId',
+      onDelete: 'CASCADE',
+      as: 'product'
+    });
+  };
+  return Review;
+};
