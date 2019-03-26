1aafbaf7b0f357b28ec6785620e136891e56c021
Olatunji Yusuf <olatunjiyso@yahoo.com>
2019-03-27 00:51:48 +0100

    add controller for filtering products

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,6 +1,6 @@
 import db from '../models/index';
 
-const { Product } = db;
+const { Product, ProductCategory, Category } = db;
 
 /**
  * Products controller to handle
@@ -39,9 +39,107 @@ class ProductsController {
           })
       });
   }
+
+
+  /**
+   * @description Fetches products by category
+   * 
+   * @param { object } req -request object
+   * @param { object } res -response object
+   * 
+   * @return { json }  message
+   */
+  static getCategoryProducts(req, res) {
+    const filter = {};
+    let foundProducts = [];
+    if (req.query.category_id) {
+      filter.categoryId = req.query.category_id;
+    }
+    ProductCategory
+      .findAll({
+        where: filter
+      })
+      .then((categoryProducts) => {
+        if(categoryProducts.length == 0) {
+          return res.status(404)
+          .json({
+            message: 'no such category exist'
+          })
+        }
+        let productIdArray = [];
+        categoryProducts.map((categoryProduct) => {
+          productIdArray.push(categoryProduct.productId)
+        });
+        Product
+          .findAll({
+            where: {
+              id: productIdArray
+            }
+          })
+          .then((products) => {
+            foundProducts = products;
+          })
+          .then(() => {
+            Category
+              .findOne({
+                where: {
+                  id: req.query.category_id
+                }
+              })
+              .then((category) => {
+                res.status(200)
+                  .json({
+                    message: 'products found!',
+                    category: category.name,
+                    products: foundProducts
+                  })
+              })
+          })
+          .catch((err) => {
+            res.status(500)
+            .json({
+              error: err.message
+            })
+          })
+      })
+  }
+
+
+  /**
+   * @description Fetches all categories in a department
+   * 
+   * @param { object } req -request object
+   * @param { object } res -response object
+   * 
+   * @return { json }  message
+   */
+  static getCategories(req, res) {
+    Category
+      .findAll({
+      where: {
+        departmentId: req.query.department_id
+      }
+      })
+      .then((categories) => {
+        if(categories.length == 0) {
+          return res.status(404)
+          .json({
+            message: 'no such department exist'
+          })
+        }
+          return res.status(200)
+            .json({
+              message: 'categories found!',
+              categories,
+            })
+      })
+      .catch((err) => {
+        res.status(500)
+          .json({
+            error: err
+          })
+      });
+  }
 }
-
-
-
 
 export default ProductsController;
