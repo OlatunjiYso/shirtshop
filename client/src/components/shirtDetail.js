03ea2167984fb2f899c8a1c50fc2ec506f3de41d
Olatunji Yusuf <olatunjiyso@yahoo.com>
2019-04-18 21:58:12 +0100

    create stateful product details page

--- client/src/components/shirtDetail.js
+++ client/src/components/shirtDetail.js
@@ -1,95 +1,127 @@
 import React from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import sampleShirt from '../images/shirt.png';
 
-const shirtDetail = () => {
+const shirtDetail = (props) => {
+  const {
+    chosenColor,
+    chosenSize,
+    shirtDetails,
+    chosenQuantity,
+    setChosenColor,
+    setChosenSize,
+    setChosenQuantity } = props;
+  if (shirtDetails) {
+    // Fetch shirt parameters
+    const { name, id, image, price, discountedPrice, description } = shirtDetails.foundProduct;
+    const { attributes } = shirtDetails;
+
+    let availableColors = [];
+    let availableSizes = [];
+    attributes.map((attribute) => {
+      if (attribute['Size']) {
+        availableSizes.push(attribute['Size'])
+      }
+      if (attribute['Color']) {
+        availableColors.push(attribute['Color'])
+      }
+    })
+
+    // Generate color blocks
+    const colorOptions = availableColors.map((color) => {
+      return (
+     <div className="colorCheckBox" id={`color${color}`} key={color} value={color} onClick={()=> setChosenColor(color)}>
+      </div>
+      )
+    })
+
+    // Generate size blocks
+    const sizesOptions = availableSizes.map((size) => {
+      return (
+        <h6 className="sizeCheckBox" key={size} value={size} onClick={() => setChosenSize(size)}>
+          {size}
+        </h6>
+      )
+    })
+    return (
+      <div className="shirtDetailBody">
+        {/* Left side */}
+        <div className="imageSection">
+          <h1 className="imageTitle"> {name} </h1>
+          <img src={require(`../images/products/${image}`)} />
+        </div>
+        {/* Right side */}
+        <div className="attributeSection">
+          <p className="itemDescription">
+            {description}
+          </p>
+          <h6 className="shirtPrice">
+            Price:
+            <span id="shirtActualValue">  ${price} </span>
+            <span className="greyText"> / piece </span>
+          </h6>
+          <h6 className="shirtPrice">
+            Discount Price:
+          <span id="shirtDiscountValue"> ${discountedPrice} </span>
+            <span className="greyText"> / piece </span>
+            <span id="discountPercentage"> - {((price - discountedPrice) / price).toFixed(1) * 100}%</span>
+          </h6>
+          <div className="coreAttributes">
+
+            <div className="specificAttributeSection">
+              <span className="colorSelectionLabel"> Color: </span>
+              {colorOptions}
+            </div>
+            {/* select color */}
+            <div className="specificAttributeSection">
+              <h6 className="sizeSelectionLabel"> Sizes: </h6>
+              {sizesOptions}
+            </div>
+            {/* Select quantity */}
+            <div className="specificAttributeSection">
+              <h6 className="sizeSelectionLabel"> Quantity: </h6>
+              <input
+                value={chosenQuantity}
+                id="selectedQuantity"
+                type="number"
+                min="1"
+                max="20"
+                name="chosenQuantity"
+                onChange={setChosenQuantity} />
+            </div>
+            {/* Display User selectiions */}
+            <div className="specificAttributeSection">
+              <h6 className="sizeSelectionLabel"> Your choice: </h6>
+              <h6 className="sizeCheckBox" id="s">
+                {chosenSize}
+              </h6>
+              <div className="colorSelected" id={`color${chosenColor}`}>
+              </div>
+              <h6 id="selectedColorText"> {chosenQuantity} No(s)</h6>
+            </div>
+          </div>
+          <div className="shirtActionButtons">
+            <button id="buyNowButton" className="payButton" type="button"> Buy Now </button>
+            <button id="addToCartButton" className="payButton" type="button"> Add to Cart </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
   return (
-    <div className="shirtDetailBody">
-      <div className="imageSection">
-        <h1 className="imageTitle"> {'<Shirt Title>'} </h1>
-        <img src={sampleShirt} />
-      </div>
-      <div className="attributeSection">
-        <p className="itemDescription">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-         </p>
-        <h6 className="shirtPrice">
-          Price:
-          <span id="shirtActualValue">  {'$150.00'} </span>
-          <span className="greyText"> / piece </span>
-        </h6>
-        <h6 className="shirtPrice">
-          Discount Price:
-        <span id="shirtDiscountValue"> {'$200.00'} </span>
-          <span className="greyText"> / piece </span>
-          <span id="discountPercentage"> - {((200 - 150) / 200) * 100}%</span>
-        </h6>
-        <div className="coreAttributes">
-          <div className="specificAttributeSection">
-            <span className="colorSelectionLabel"> Color: </span>
-            <div className="colorCheckBox" id="whiteColor">
-            </div>
-            <div className="colorCheckBox" id="blackColor">
-            </div>
-            <div className="colorCheckBox" id="redColor">
-            </div>
-            <div className="colorCheckBox" id="orangeColor">
-            </div>
-            <div className="colorCheckBox" id="yellowColor">
-            </div>
-            <div className="colorCheckBox" id="greenColor">
-            </div>
-            <div className="colorCheckBox" id="blueColor">
-            </div>
-            <div className="colorCheckBox" id="indigoColor">
-            </div>
-            <div className="colorCheckBox" id="purpleColor">
-            </div>
-          </div>
-          <div className="specificAttributeSection">
-            <h6 className="sizeSelectionLabel"> Sizes: </h6>
-            <h6 className="sizeCheckBox" id="s">
-              Small
-            </h6>
-            <h6 className="sizeCheckBox" id="m">
-              Medium
-            </h6>
-            <h6 className="sizeCheckBox" id="l">
-              Large
-            </h6>
-            <h6 className="sizeCheckBox" id="xl">
-              X-Large
-            </h6>
-            <h6 className="sizeCheckBox" id="xxl">
-              XX-Large
-            </h6>
-          </div>
-          <div className="specificAttributeSection">
-            <h6 className="sizeSelectionLabel"> Quantity: </h6>
-            <input id="selectedQuantity" type="number" min="1" max="20" />
-          </div>
-          <div className="specificAttributeSection">
-            <h6 className="sizeSelectionLabel"> Your choice: </h6>
-            <h6 className="sizeCheckBox" id="s">
-              Small
-            </h6>
-            <div className="colorSelected" id="yellowColor">
-            </div>
-            <h6 id="selectedColorText"> { 20 } No(s)</h6>
-          </div>
-        </div>
-        <div className="shirtActionButtons">
-          <button id="buyNowButton" className="payButton" type="button"> Buy Now </button>
-          <button id="addToCartButton" className="payButton" type="button"> Add to Cart </button>
-        </div>
-      </div>
-    </div>
+    <h3> Please wait . . . . . .  </h3>
   )
 }
 
 shirtDetail.propTypes = {
-  name: propTypes.string,
+  shirtDetails: PropTypes.object,
+  chosenColor: PropTypes.string.isRequired,
+  chosenQuantity: PropTypes.any.isRequired,
+  chosenSize: PropTypes.string.isRequired,
+  setChosenColor: PropTypes.func.isRequired,
+  setChosenQuantity: PropTypes.func.isRequired,
+  setChosenSize: PropTypes.func.isRequired
 };
 
 export default shirtDetail;
